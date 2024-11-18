ae913319d03d9bdfb547d3bd215f3b50640a23eb
Thiago Henrique <codedemonbr@gmail.com>
2024-11-17 22:31:41 -0300

    Spring Boot com configurações iniciais

---
+++ src/main/java/com/codedemonbr/rest_api_bootcamp/RestApiBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.codedemonbr.rest_api_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codedemonbr/rest_api_bootcamp/RestApiBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedemonbr.rest_api_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
